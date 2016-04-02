7a05e7e85063d45dd3e198bf1a1dfbc753074927
Sam Corder <samus@codeargyle.com>
2016-04-01 21:25:00 -0400

    Initial Commit

---
+++ ReRxSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReRxSwift
+//
+//  Created by Sam Corder on 4/1/16.
+//  Copyright © 2016 Synapptic Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
