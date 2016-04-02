631370441942064a5130aa70c188fd750cfc0302
Sam Corder <samus@codeargyle.com>
2016-04-02 11:18:38 -0400

    1. Lots of state wrangling

--- ReRxSwift/ViewController.swift
+++ ReRxSwift/ViewController.swift
@@ -9,17 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var outputLabel: UILabel!
+    @IBOutlet weak var countLabel: UILabel!
 
+    let model = ModelObject()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        textField.addTarget(self, action: #selector(textFieldChanged), forControlEvents: UIControlEvents.EditingChanged)
+        textField.addTarget(self, action: #selector(textFieldChanged), forControlEvents: UIControlEvents.ValueChanged)
+        outputLabel.text = ""
+        countLabel.text = "0"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func textFieldChanged(sender: AnyObject) {
+        model.text = textField.text
+        outputLabel.text = model.uppercased
+        countLabel.text = String(model.charCount)
     }
-
+    
+    @IBAction func outOfBandTapped(sender: AnyObject) {
+        let someStr = "Out of band tapped"
+        textField.text = someStr
+        model.text = someStr
+        outputLabel.text = model.uppercased
+        countLabel.text = String(model.charCount)
+    }
 
 }
 
+@objc class ModelObject: NSObject {
+    var text: String? {
+        didSet {
+            if let text = text {
+                uppercased = text.uppercaseString
+                charCount = text.characters.count
+            } else {
+                uppercased = nil
+            }
+        }
+    }
+    dynamic private (set) var charCount = 0
+    dynamic private (set) var uppercased: String?
+    
+}
